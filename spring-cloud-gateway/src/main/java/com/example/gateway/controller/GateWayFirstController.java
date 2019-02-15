3b56aa7f089dddffafb1557a11f4b153d2c4effe
yantao <yant09@vanke.com>
2019-02-15 10:27:44 +0800

    新增restController value注解的解释

--- spring-cloud-gateway/src/main/java/com/example/gateway/controller/GateWayFirstController.java
+++ spring-cloud-gateway/src/main/java/com/example/gateway/controller/GateWayFirstController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//注意@RestController（value = "gateway"） 并不能使路径带上 /gateway
-//value ="/gateway" 和 value ="gateway" 一样的 ，即 带不带/ 效果都一样
+/*
+@RestController 的value 属性的意思：The value may indicate a suggestion for a logical component name,
+注意@RestController（value = "gateway"） 并不能使路径带上 /gateway， 所以要使用 @RequestMapping(value = "/gateway")
+RequestMapping value ="/gateway" 和 value ="gateway" 一样的 ，即 带不带/ 效果都一样
+*/
 @RestController
 @RequestMapping(value = "/gateway")
 public class GateWayFirstController {
